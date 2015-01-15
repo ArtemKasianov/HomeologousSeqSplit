0f341c495169fb90c68ddb67ce91f1980f4ee231
ArtemKasianov <artem.kasianov@gmail.com>
2015-01-15 07:29:02 +0300

    Create adj_matrix.h

---
+++ adj_matrix.h
@@ -0,0 +1,23 @@
+#ifndef ADJ_MATRIX_H
+#define ADJ_MATRIX_H
+
+#include "BaseClasses.h"
+
+class Adj_matrix{
+    private:
+        SetOf<SetOf<int> > _matrix;
+    public:
+        Adj_matrix():_matrix()
+        {
+
+        }
+
+
+
+
+
+};
+
+
+
+#endif // ADJ_MATRIX_H
